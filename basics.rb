4c1242103f3b1f5b4c9415110bc59691a3add377
TGOlson <tydotg@gmail.com>
2013-10-02 13:43:19 -0500

    new heroku name

--- basics.rb
+++ basics.rb
@@ -3,23 +3,11 @@ require 'sinatra'
 require 'integer_expansion'
 
 get '/' do
-  "Hello, World!"	
-end
-
-get '/about' do 
-  "A litte about me."
-end
-
-get '/hello/:name/:city' do 
-  "Hello there, #{params[:name].capitalize} from #{params[:city].capitalize}."
-end
-
-get '/english_number' do 
   @title = 'English Numbers' 
   erb :english_number
 end
 
-post '/english_number' do 
+post '/' do 
   @title = 'English Numbers'
   @num = params[:number].to_i.add_commas
   @enum = "#{params[:number].to_i.to_english}"
